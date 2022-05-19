d5c61298cbf62e0337caea63ecaab241efee180b
Kristof Loneck <kristof.loneck@liferay.com>
2022-05-19 11:07:14 +0200

    LPS-153928: Checking if target folder removed before moving files

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -3515,7 +3515,9 @@ public class DLFileEntryLocalServiceImpl
 			DLFolder parentDLFolder = _dlFolderPersistence.findByPrimaryKey(
 				folderId);
 
-			if (groupId != parentDLFolder.getGroupId()) {
+			if ((groupId != parentDLFolder.getGroupId()) ||
+				parentDLFolder.isInTrash()) {
+
 				throw new NoSuchFolderException();
 			}
 		}
